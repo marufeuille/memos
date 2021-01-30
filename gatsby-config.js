25502cf0397b294c91ee23c73d0b2a3537b39968
Masahiro Ishii <marufeuille@gmail.com>
2021-01-30 17:56:56 +0900

    initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-theme-blog`,
+      options: {},
+    },
+  ],
+  // Customize your site metadata:
+  siteMetadata: {
+    title: `memo`,
+    author: `marufeuille`,
+    description: `this is my memos`,
+    social: [
+      {
+        name: `twitter`,
+        url: `https://twitter.com/marufeuille`,
+      },
+      {
+        name: `github`,
+        url: `https://github.com/marufeuille`,
+      },
+    ],
+  },
+}
---
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { Styled } from "theme-ui"
+
+/**
+ * Change the content to add your own bio
+ */
+
+export default function Bio() {
+  return (
+    <>
+      marufeuille
+      <br />
+      DataEngineer
+    </>
+  )
+}
